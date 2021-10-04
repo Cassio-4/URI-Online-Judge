571b2c181bbf864956a0b902b24b01c05985ec9e
Cassio-4 <cassiobertasinascimento@gmail.com>
2021-10-04 20:14:55 -0300

    Begginer 1073

--- c++/Begginer/1073.cpp
+++ c++/Begginer/1073.cpp
@@ -7,10 +7,8 @@ int main() {
     uint i, pot;
     cin >> N;
     for(i = 2; i <= N; i=i+2) {
-        if(i % 2 == 0) {
-            pot = i * i;
-            cout << i << "^" << 2 << " = " << pot << endl;
-        }
+        pot = i * i;
+        cout << i << "^" << 2 << " = " << pot << endl;
     }
     return 0;
 }
